fea5ae8112c755f7c92316b4443eb9efa977893d
oRorsch <94205975+oRorsch@users.noreply.github.com>
2022-05-16 16:36:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Lorem Ipsum</title>
+  </head>
+  <body>
+                                          <!-- parte da barra de navegação -->
+    <div class="container">
+      <form>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Blame</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+<div class="mx-auto" style="width: 450px;">
+
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Promoções</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Destaques</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Preços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contato</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </form>
+</div>
+
+                                                <!-- Espaçando a Navbar -->
+
+    <div class="container-fluid">
+
+    <div class="mx-auto" style="width: 1000px">
+
+      <div class="row">
+      
+        <div class="col-12"><div class="p-3 mb-2 bg-dark text-white">IMPERDÍVEIS</div></div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+                                                      <!-- Cards -->
+
+    <div class="container-fluid">
+      <div class="mx-auto" style="width: 950px;">
+        <div class="row">
+          <div class="col-4">
+
+          <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://st2.depositphotos.com/4164031/6914/i/450/depositphotos_69145633-stock-photo-flag-of-brazil.jpg/100px180/" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Lorem Ipsum</h5>
+    <p class="card-text">dolor sit amet, consectetur adipiscing elit. In tincidunt, massa eget vulputate auctor, dui lectus malesuada ex, id sagittis arcu orci nec arcu. Vivamus vestibulum sed metus ac porttitor. In hac habitasse platea dictumst.</p>
+    <a href="#" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+
+          </div>
+          <div class="col-4">
+
+          <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://st2.depositphotos.com/4164031/6914/i/450/depositphotos_69145633-stock-photo-flag-of-brazil.jpg/100px180/" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Ipsum Lorem</h5>
+    <p class="card-text">dolor sit amet, consectetur adipiscing elit. In tincidunt, massa eget vulputate auctor, dui lectus malesuada ex, id sagittis arcu orci nec arcu. Vivamus vestibulum sed metus ac porttitor. In hac habitasse platea dictumst.</p>
+    <a href="#" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+
+          </div>
+          <div class="col-4">
+          
+          <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://st2.depositphotos.com/4164031/6914/i/450/depositphotos_69145633-stock-photo-flag-of-brazil.jpg/100px180/" alt="Imagem de capa do card">
+  <div class="card-body">
+    <h5 class="card-title">Lopsum Irem</h5>
+    <p class="card-text">dolor sit amet, consectetur adipiscing elit. In tincidunt, massa eget vulputate auctor, dui lectus malesuada ex, id sagittis arcu orci nec arcu. Vivamus vestibulum sed metus ac porttitor. In hac habitasse platea dictumst.</p>
+    <a href="#" class="btn btn-primary">Visitar</a>
+  </div>
+</div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+                                               <!--espaçando das imagens-->
+    
+  <div class="container-fluid">
+
+    <div class="mx-auto" style="width: 1000px">
+
+      <div class="row">
+      
+        <div class="col-12"><div class="p-3 mb-2 bg-info text-white">VEJA NOSSOS PINGUINS</div></div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+                                                      <!-- Listas -->
+    <div class="container-fluid">
+        
+        <div class ="mx-auto" style="width: 1000px;">
+          
+          <div class="row">
+            
+            <div class="col-12">
+              
+              <ul class="list-unstyled">
+  <li class="media">
+    <img class="mr-3" src="https://img.r7.com/images/top-100-hora-7-errado-google-24092021021515245?dimensions=100x180&no_crop=true/100x180" alt="Imagem de exemplo genérica">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Pinguinzinho com espadinha!</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media my-4">
+    <img class="mr-3" src="https://img.r7.com/images/top-100-hora-7-errado-google-24092021021515245?dimensions=180x200&no_crop=true/100x180" alt="Imagem de exemplo genérica">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Pinguim com espada!</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+  <li class="media">
+    <img class="mr-3" src="https://img.r7.com/images/top-100-hora-7-errado-google-24092021021515245?dimensions=260x220&no_crop=true/100x180" alt="Imagem de exemplo genérica">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Pinguinzão com espadão!</h5>
+      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    </div>
+  </li>
+</ul>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+                                           <!-- Espaçando os Pinguins -->
+
+<div class="container-fluid">
+
+    <div class="mx-auto" style="width: 1000px">
+
+      <div class="row">
+      
+        <div class="col-12"><div class="p-3 mb-2 bg-danger text-white">VEJA TAMBÉM</div></div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+                                              <!-- Imagens do site -->
+      <div class="container-fluid">
+
+        <div class="mx-auto" style="width: 1000px;">
+
+          <div class="row">
+            
+            <div class="col-6">
+
+              <figure class="figure">
+  <img src="https://st2.depositphotos.com/6544740/9337/i/600/depositphotos_93376372-stock-photo-sunset-over-sea-pier.jpg/600x400" class="figure-img img-fluid rounded" alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
+  <figcaption class="figure-caption"><h3>Lorem Ipsum Dolor Amet</h3></figcaption>
+</figure>
+
+            </div>
+
+            <div class="col-6">
+              
+              <img src="https://i0.wp.com/multarte.com.br/wp-content/uploads/2015/08/imagens-amor.jpg?fit=1680%2C1050&ssl=1/600x400" class="figure-img img-fluid rounded" alt="Imagem de um quadrado genérico com bordas arredondadas, em uma figure.">
+  <figcaption class="figure-caption"><h3>Consectetur Adipiscing Elit</h3></figcaption>
+</figure>
+
+            </div>
+
+          </div>
+          
+        </div>
+        
+      </div>
+
+                                              <!-- Espaçando as Imagens -->
+
+    <div class="container-fluid">
+
+    <div class="mx-auto" style="width: 1000px">
+
+      <div class="row">
+      
+        <div class="col-12"><div class="p-3 mb-2 bg-secondary text-white">CADASTRE-SE</div></div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+                                                      <!-- Cadastro -->
+      <div class="container-fluid">
+        
+        <div class ="mx-auto" style="width: 800px;">
+          
+          <div class="row">
+            
+            <div class="col-12">
+              
+              <div class="p-3 mb-2 bg-secondary text-white">
+
+                <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Endereço de email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email">
+    <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Concorda com os termos de uso?</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+  </body>
+</html>
